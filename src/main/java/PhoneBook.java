ded0f845105885fba1b57895e4d090464a3dd65a
KomX <komx512@outlook.com>
2025-08-10 12:51:50 +0500

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,4 +1,7 @@
 public class PhoneBook {
 
+    int add(String name, String number){
 
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    private PhoneBook testBook;
+
+    @BeforeAll
+    static void startTest() {
+        System.out.println("Начало тестирования...");
+    }
+
+    @BeforeEach
+    void addressInit() {
+        testBook = new PhoneBook();
+    }
+
+    @Test
+    void testAddMethod() {
+        String[] names = {"Василий", "Петр", "Колбасный", "Кварц-15"};
+        String[] numbers = {"2223312", "123456", "234-654", "150015"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Добавляем " + names[i] + " " + numbers[i]);
+            assertEquals(i+1, testBook.add(names[i], numbers[i]));
+        }
+    }
+
+    @AfterEach
+    void endTest() {
+        System.out.println("Тестирование завершено!");
+    }
 }
